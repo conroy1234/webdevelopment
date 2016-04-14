2189a34cf2ca978d826eb8e22523c160652f24bb
whiteco <whiteco@WIMPC726A.domgen.corp>
2016-04-14 15:01:52 +0100

    make some updates on the project

---
+++ src/main/java/com/domgen/application/ApplicationInfoFile.java
@@ -0,0 +1,20 @@
+package com.domgen.application;
+
+import javax.annotation.ManagedBean;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+@Named
+@ManagedBean
+@Scope("Session")
+public interface ApplicationInfoFile {
+
+	String welcome_display="SPRING APPLICATION CONFIGERATION";
+	String name="thes is the begining of my spring project";
+	String welcome_messahe="WELCOME TO SPRING MVC THIS IS BEGINING OF MY LEARNING";
+	String forward_to_index_page="index";
+	
+	
+	
+}
